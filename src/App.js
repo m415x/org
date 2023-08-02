e7b1f59eb2d818e5cd0018a237b21aee616879c4
Cristian Lahoz <lahozcristian@gmail.com>
2023-08-02 12:17:08 -0300

    clase 3a

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import './App.css';
 import Header from './components/Header/Header';
-
+import Form from './components/Form/Form';
 
 function App() {
   return (
     <div>
       <Header />
-      
+      <Form />
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/Form/Form.js
@@ -0,0 +1,13 @@
+import "./Form.css"
+
+const Form = () => {
+  return (
+    <section className="form">
+      <form>
+        <h2>Rellena el formulario para crear el colaborador</h2>
+      </form>
+    </section>
+  );
+};
+
+export default Form;
